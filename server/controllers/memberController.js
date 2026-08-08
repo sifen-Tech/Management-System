6cf396ced53bb681bb67dd683bdb99903be60385
unknown <sifenbeyan@gmail.com>
2026-08-08 23:09:45 +0300

    commit

--- server/controllers/memberController.js
+++ server/controllers/memberController.js
@@ -24,6 +24,95 @@ const createMember = async (req, res) => {
   }
 };
 
+const getAllMembers = async (req, res) => {
+  try {
+    const members = await Member.find().populate(
+      "createdBy",
+      "fullName email role",
+    );
+
+    res.status(200).json({
+      message: "Members retrieved successfully",
+      members,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+// Get member by ID
+const getMemberById = async (req, res) => {
+  try {
+    const member = await Member.findById(req.params.id).populate(
+      "createdBy",
+      "fullName email role",
+    );
+
+    if (!member) {
+      return res.status(404).json({
+        message: "Member not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Member retrieved successfully",
+      member,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const updateMember = async (req, res) => {
+  try {
+    const member = await Member.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    }).populate("createdBy", "fullName email role");
+
+    if (!member) {
+      return res.status(404).json({
+        message: "Member not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Member updated successfully",
+      member,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+const deleteMember = async (req, res) => {
+  try {
+    const member = await Member.findByIdAndDelete(req.params.id);
+
+    if (!member) {
+      return res.status(404).json({
+        message: "Member not found",
+      });
+    }
+
+    res.status(200).json({
+      message: "Member deleted successfully",
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
 module.exports = {
   createMember,
+  getAllMembers,
+  getMemberById,
+  updateMember,
+  deleteMember,
 };
--- server/routes/memberRoutes.js
+++ server/routes/memberRoutes.js
@@ -5,14 +5,36 @@ const router = express.Router();
 const authMiddleware = require("../middleware/authMiddleware");
 const authorize = require("../middleware/roleMiddleware");
 
-const { createMember } = require("../controllers/memberController");
-
-// Create member
+const {
+  createMember,
+  getAllMembers,
+  getMemberById,
+  updateMember,
+  deleteMember,
+} = require("../controllers/memberController");
 router.post(
   "/",
   authMiddleware,
   authorize("admin", "supervisor"),
   createMember,
 );
-
+router.get(
+  "/",
+  authMiddleware,
+  authorize("admin", "supervisor", "user"),
+  getAllMembers,
+);
+router.get(
+  "/:id",
+  authMiddleware,
+  authorize("admin", "supervisor", "user"),
+  getMemberById,
+);
+router.put(
+  "/:id",
+  authMiddleware,
+  authorize("admin", "supervisor"),
+  updateMember,
+);
+router.delete("/:id", authMiddleware, authorize("admin"), deleteMember);
 module.exports = router;
