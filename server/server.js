b175a87f839959f312f1174fe07870e788a719ae
unknown <sifenbeyan@gmail.com>
2026-08-11 19:42:07 +0300

    commit

--- server/server.js
+++ server/server.js
@@ -4,19 +4,28 @@ require("dotenv").config();
 
 const connectDB = require("./config/db");
 const errorMiddleware = require("./middleware/errorMiddleware");
+
 const authRoutes = require("./routes/authRoutes");
 const memberRoutes = require("./routes/memberRoutes");
 const attendanceRoutes = require("./routes/attendanceRoutes");
-
 const userRoutes = require("./routes/userRoutes");
-
-connectDB();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+connectDB();
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      process.env.CLIENT_URL,
+    ].filter(Boolean),
+    credentials: true,
+  }),
+);
 
 app.use(express.json());
 
@@ -26,7 +35,10 @@ app.use("/api/attendance", attendanceRoutes);
 app.use("/api/users", userRoutes);
 
 app.get("/api", (req, res) => {
-  res.send("Role-Based Management System API is running...");
+  res.status(200).json({
+    success: true,
+    message: "Role-Based Management System API is running...",
+  });
 });
 
 app.use(errorMiddleware);
